8c3e1361144e57ac6f2e9776f284de0b1536aaea
David Rohweder <david01rohweder@gmail.com>
2023-04-08 14:42:00 -0400

    better base

--- code/models/train_model.py
+++ code/models/train_model.py
@@ -2,25 +2,13 @@ import pandas as pd
 import tensorflow as tf
 import pickle
 
-def train_model(X_train, X_test, y_train, y_test, scaler):
+def train_model(X_train, X_test, y_train, y_test, scaler):    
+    model = tf.keras.Sequential([tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)), tf.keras.layers.Dense(32, activation='relu'), tf.keras.layers.Dense(1, activation='sigmoid')]) # create / define the model || subject to changes
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) # compile
+    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2) # train model
+    test_loss, test_accuracy = model.evaluate(X_test, y_test) # eval the model
+    print(f'Test accuracy: {test_accuracy}, Test loss: {test_loss}') # is our model trash
 
-    # Define the neural network model
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
-
-    # Compile the model
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    # Train the model
-    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2)
-
-    # Evaluate the model
-    test_loss, test_accuracy = model.evaluate(X_test, y_test)
-    print(f'Test accuracy: {test_accuracy}')
-
-    model.save('./models/loan_prediction_model.h5')
-    with open('./models/scaler.pkl', 'wb') as f:
-        pickle.dump(scaler, f)
+    model.save('./models/loan_prediction_model.h5') # save our model
+    with open('./models/scaler.pkl', 'wb') as f: 
+        pickle.dump(scaler, f) # data the scalar data to the file
--- code/preprocessing/standardization/preprocessing.py
+++ code/preprocessing/standardization/preprocessing.py
@@ -4,37 +4,30 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 def preprocess_raw_data():
     data = pd.read_csv('./preprocessing/raw_data/training_data.csv') # Load and preprocess the data
-    data['Loan_Status'] = data['Loan_Status'].map({'Y': 1, 'N': 0})
-
+    data['Loan_Status'] = data['Loan_Status'].map({'Y': 1, 'N': 0}) # data given as Y/N map to corresponding 1 and 0's
     label_encoders = {}
-
-    categorical_columns = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area'] # Encoding categorical columns
+    categorical_columns = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area'] # encoding categorical columns
     for col in categorical_columns:
         label_encoders[col] = LabelEncoder()
         label_encoders[col].fit(data[col].astype(str))
 
-    # Encoding categorical columns in the new data
     for col in categorical_columns:
         data[col] = label_encoders[col].transform(data[col].astype(str))
 
-    # Fill missing values in numerical columns with their mean
     numerical_columns = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History']
     for col in numerical_columns:
         data[col].fillna(data[col].mean(), inplace=True)
 
-    # Fill missing values in categorical columns with their mode
     for col in categorical_columns:
         data[col].fillna(data[col].mode()[0], inplace=True)
 
-    # Split data into features (X) and target (y)
-    X = data.drop(['Loan_ID', 'Loan_Status'], axis=1)
-    y = data['Loan_Status']
+    measured_fields = data.drop(['Loan_ID', 'Loan_Status'], axis=1)  # Split data into features (X) and target (y)
+    target_field = data['Loan_Status']
 
     # Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(measured_fields, target_field, test_size=0.2, random_state=42)
 
-    # Scale the numerical features
-    scaler = StandardScaler()
+    scaler = StandardScaler() # Scale the numerical features
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
