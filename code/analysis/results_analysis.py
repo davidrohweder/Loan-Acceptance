cde2a407aca08c6d9b331882cb76958af8579496
mohil270 <98039067+mohil270@users.noreply.github.com>
2023-04-28 14:32:19 -0400

    Update results_analysis.py
    added more stats

--- code/analysis/results_analysis.py
+++ code/analysis/results_analysis.py
@@ -30,4 +30,30 @@ def data_analysis(statistic=1):
         upper_bound = low_category_mean + margin_of_error
         print(f"Confidence Intervals (95%): ({lower_bound}, {upper_bound})")
 
-    # todo more stats >:)
+    # case 3: Box plot of LoanAmount by Property_Area
+    elif statistic == 3:
+        plt.figure(figsize=(8, 6))
+        sns.boxplot(x='Property_Area', y='LoanAmount', data=data)
+        plt.title('Box Plot of LoanAmount by Property Area')
+        plt.xlabel('Property Area')
+        plt.ylabel('Loan Amount')
+        plt.show()
+
+    #case 4: # Scatter plot of ApplicantIncome vs. CoapplicantIncome
+    elif statistic == 4:
+        plt.figure(figsize=(8, 6))
+        sns.scatterplot(x='ApplicantIncome', y='CoapplicantIncome', data=data)
+        plt.title('Scatter Plot of ApplicantIncome vs. CoapplicantIncome')
+        plt.xlabel('Applicant Income')
+        plt.ylabel('Coapplicant Income')
+        plt.show()
+
+    #case 5 : Count plot of Loan_Amount_Term
+    elif statistic == 5:
+        plt.figure(figsize=(8, 6))
+        sns.countplot(x='Loan_Amount_Term', data=data)
+        plt.title('Count Plot of Loan Amount Term')
+        plt.xlabel('Loan Amount Term')
+        plt.ylabel('Count')
+        plt.show()
+    
